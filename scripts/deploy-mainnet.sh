0c136c3eddd7e7747b526722cc981392fbb4a4f4
golden-0815 <andreas.larsen.062410@gmail.com>
2026-04-21 17:23:01 +0900

    feat: added the deploy mainnet

---
+++ scripts/deploy-mainnet.sh
@@ -0,0 +1,42 @@
+set -e
+NETWORK=mainnet
+SUFFIX=near
+export NODE_ENV=$NETWORK
+
+OWNER=narwallets.$SUFFIX
+MASTER_ACC=meta-pool.$SUFFIX
+CONTRACT_ACC=$MASTER_ACC
+GOV_TOKEN=meta-token.$SUFFIX
+OPERATOR_ACC=operator.meta-pool.$SUFFIX
+TREASURY_ACC=treasury.meta-pool.$SUFFIX
+
+#meta --cliconf -c $CONTRACT_ACC -acc $OWNER
+
+# near create-account $OPERATOR_ACC --masterAccount $MASTER_ACC --accountId $OWNER --initialBalance 0.5
+# near create-account $TREASURY_ACC --masterAccount $MASTER_ACC --accountId $OWNER --initialBalance 0.5
+
+## delete acc
+#echo "Delete $CONTRACT_ACC? are you sure? Ctrl-C to cancel"
+#read input
+#near delete $CONTRACT_ACC $MASTER_ACC
+set -ex
+near deploy $CONTRACT_ACC ./res/metapool.wasm
+# near deploy $CONTRACT_ACC ./res/metapool.wasm \
+#   --initFunction "new" \
+#   --initArgs "{\"owner_account_id\":\"$OWNER\",\"treasury_account_id\":\"$TREASURY_ACC\",\"operator_account_id\":\"$OPERATOR_ACC\",\"meta_token_account_id\":\"$GOV_TOKEN\"}" \
+#   --accountId $OWNER
+
+#meta set_params
+#near call $CONTRACT_ACC set_busy "{\"value\":false}" --accountId preprod-pool.testnet --depositYocto 1
+## deafult 4 pools
+##meta default_pools_testnet
+
+## redeploy code only
+# near deploy $CONTRACT_ACC ./res/metapool.wasm  --accountId $MASTER_ACC
+#meta set_params
+
+#save this deployment  (to be able to recover state/tokens)
+set -ex
+mkdir -p res/mainnet/metapool
+cp res/metapool.wasm res/mainnet/metapool.$CONTRACT_ACC.`date +%F.%T`.wasm
+date +%F.%T
