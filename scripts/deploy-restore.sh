62a69aa0ba65f9e4b649fbf5469fbb9f87f6a4d7
golden-0815 <andreas.larsen.062410@gmail.com>
2026-04-21 17:24:39 +0900

    feat: added deploy test net files

---
+++ scripts/deploy-restore.sh
@@ -0,0 +1,28 @@
+set -e
+NETWORK=testnet
+OWNER=lucio.$NETWORK
+MASTER_ACC=pool.$NETWORK
+CONTRACT_ACC=meta.$MASTER_ACC
+
+meta --cliconf -c $CONTRACT_ACC -acc $OWNER
+
+export NODE_ENV=$NETWORK
+
+## delete acc
+#echo "Delete $CONTRACT_ACC? are you sure? Ctrl-C to cancel"
+#read input
+#near delete $CONTRACT_ACC $MASTER_ACC
+#near create-account $CONTRACT_ACC --masterAccount $MASTER_ACC
+#meta deploy ./res/metapool.wasm
+#meta new { owner_account_id:$OWNER, treasury_account_id:treasury.$CONTRACT_ACC, operator_account_id:operator.$CONTRACT_ACC} --accountId $MASTER_ACC
+## set params@meta set_params
+#meta default_pools_testnet
+
+
+## redeploy code only
+#meta deploy ./res/metapool.wasm  --accountId $MASTER_ACC
+meta deploy ./res/metapool.2021-04-13.02:49:21.wasm
+
+#save last deployment  (to be able to recover state/tokens)
+#cp ./res/metapool.wasm ./res/metapool.`date +%F.%T`.wasm
+#date +%F.%T
---
+++ scripts/deploy-testnet.sh
@@ -0,0 +1,50 @@
+set -e
+NETWORK=testnet
+OWNER=lucio.$NETWORK
+MASTER_ACC=pool.$NETWORK
+OPERATOR_ACC_SUFFIX=.meta.pool.$NETWORK
+OPERATOR_ACC=operator$OPERATOR_ACC_SUFFIX
+CONTRACT_ACC=meta-v2.$MASTER_ACC
+GOV_TOKEN=token.meta.$MASTER_ACC
+
+export NEAR_ENV=$NETWORK
+
+## delete acc
+#echo "Delete $CONTRACT_ACC? are you sure? Ctrl-C to cancel"
+#read input
+#near delete $CONTRACT_ACC $MASTER_ACC
+#near create-account $CONTRACT_ACC --masterAccount $MASTER_ACC
+#meta deploy ./res/metapool.wasm
+#meta new { owner_account_id:$OWNER, treasury_account_id:treasury$OPERATOR_ACC_SUFFIX, operator_account_id:OPERATOR_ACC, meta_token_account_id:$GOV_TOKEN } --accountId $MASTER_ACC
+## set params@meta set_params
+#meta set_params
+## deafult 4 pools
+##meta default_pools_testnet
+
+## test
+#near call $CONTRACT_ACC set_busy "{\"value\":false}" --accountId $CONTRACT_ACC --depositYocto 1
+
+# set contract busy to make sure we're not upgrading in the middle of a cross-contract call
+set -ex
+bash build.sh
+#near call $CONTRACT_ACC set_busy '{"value":true}' --accountId $OPERATOR_ACC --depositYocto 1
+set -e
+
+# ## redeploy code only
+echo $NETWORK, $CONTRACT_ACC
+near deploy $CONTRACT_ACC ./res/metapool.wasm  --accountId $MASTER_ACC --networkId $NETWORK
+# ## MIGRATE ##
+echo --TRY MIGRATE--
+near call $CONTRACT_ACC migrate "{}" --accountId $OPERATOR_ACC
+echo --END TRY MIGRATE, CAN FAIL ON 2dN DEPLOY IF YOU FORGET TO CHANGE OLD_STATE, BUT THAT IS NO PROBLEM --
+
+#near call $CONTRACT_ACC set_busy '{"value":false}' --accountId $OPERATOR_ACC --depositYocto 1
+
+#near deploy contract4.preprod-pool.testnet ./res/metapool.wasm  --accountId preprod-pool.testnet
+#near call contract4.preprod-pool.testnet set_busy "{\"value\":false}" --accountId preprod-pool.testnet --depositYocto 1
+
+#save this deployment  (to be able to recover state/tokens)
+set -ex
+mkdir -p res/testnet/metapool
+cp res/metapool.wasm res/testnet/metapool/$CONTRACT_ACC.`date +%F.%T`.wasm
+date +%F.%T
